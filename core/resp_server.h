d8d7c616e1fb58bb7c25a64464153a4327c467a8
Lyuheng Yuan <lvhengy@gmail.com>
2023-11-29 21:29:49 -0600

    fix

--- core/resp_server.h
+++ core/resp_server.h
@@ -39,9 +39,9 @@ public:
                 assert(it != kvmap.end());
                 task_container * tc_new = it->second;
                 kvmap.erase(it);
-                pending_patterns_num--;
                 bucket.unlock();
                 ready_patterns.enqueue(tc_new);
+                pending_patterns_num--;
             }
         }
     }
