c400dbe03f40507c37476638ca13311c4d43ec10
Lyuheng Yuan <lvhengy@gmail.com>
2023-12-01 02:59:39 -0600

    fix

--- comper.h
+++ comper.h
@@ -1794,7 +1794,7 @@ public:
                 if (succ)
                 {
                     activate_task_container(tc_new);
-                    if (GET_PATTERN_ID(tc_new->parent_qid) != 0)
+                    // if (GET_PATTERN_ID(tc_new->parent_qid) != 0)
                         // delete_queue.add(RequestMsg{tc_new->qid, tc_new->parent_qid});
                 }
                 else 
