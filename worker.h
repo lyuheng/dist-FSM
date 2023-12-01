c1d450c65ab4ac93b61216758916b42673288b5a
Lyuheng Yuan <lvhengy@gmail.com>
2023-12-01 00:03:10 -0600

    fix

--- worker.h
+++ worker.h
@@ -291,8 +291,8 @@ public:
                 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
                 // delete every pointer in tc_vec !!!!!!!!!!!!!!!!!
                 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                for (auto it = tc_vec.begin(); it != tc_vec.end(); ++it)
-                    delete *it;
+                // for (auto it = tc_vec.begin(); it != tc_vec.end(); ++it)
+                //     delete *it;
             }
         }
         
@@ -317,8 +317,8 @@ public:
                 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
                 // delete every pointer in tc_vec !!!!!!!!!!!!!!!!!
                 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                for (auto it = tc_vec.begin(); it != tc_vec.end(); ++it)
-                    delete *it;
+                // for (auto it = tc_vec.begin(); it != tc_vec.end(); ++it)
+                //     delete *it;
             }
         }
         return true;
