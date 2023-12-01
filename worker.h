a38117f62133212924bcba8fa798678bf1d51b6c
Lyuheng Yuan <lvhengy@gmail.com>
2023-12-01 02:19:06 -0600

    fix

--- worker.h
+++ worker.h
@@ -406,5 +406,16 @@ public:
         }
 
         cout << "global map is empty: " << g_pattern_prog_map.empty() << endl;
+
+        for(int i=0; i<CONMAP_BUCKET_NUM; i++)
+    	{
+    		auto & bucket = g_pattern_prog_map.pos(i);//todo auto
+    		auto & kvmap = bucket.get_map();
+    		
+			for(auto it = kvmap.begin(); it != kvmap.end(); it++)
+			{
+				delete it->second; //release task_prog
+			}
+    	}
     }
 };
