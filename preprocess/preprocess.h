01a5966947f0b445259440e064fee1d916549d83
Lyuheng Yuan <lvhengy@gmail.com>
2023-12-07 21:26:49 -0600

    fix

--- preprocess/preprocess.h
+++ preprocess/preprocess.h
@@ -384,7 +384,7 @@ void Graph::readSnapFile(const std::string &filename, const std::string &label_f
             // if (ch != ',') assert(false);
             iss >> label;
             label_map_[nline - min_vertex_id] = label;
-            // std::cout << label << " ";
+            std::cout << label << " ";
             label_set_.insert(label);
             nline ++;
         }
