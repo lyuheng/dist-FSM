29440d7c7c0ea325b9a8a8fca952c08bac927419
Lyuheng Yuan <lvhengy@gmail.com>
2023-12-07 19:44:30 -0600

    fix

--- preprocess/preprocess.h
+++ preprocess/preprocess.h
@@ -668,7 +668,7 @@ void Graph::writeGraphFile(const std::string &filename)
         for (uintV i = 0; i < vertex_count_; ++i)
         {
             std::cout << new_labels_[i] << " ";
-            file_out << "v " << i << " " << new_labels_[i] << " " << row_ptrs_[i+1]-row_ptrs_[i] << "\n";
+            file_out << "v " << i << " " << std::to_string(new_labels_[i]) << " " << row_ptrs_[i+1]-row_ptrs_[i] << "\n";
         }
     }
     else 
