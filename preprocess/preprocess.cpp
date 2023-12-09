302b6f91f620c504067c8a4850fa22058e155389
Lyuheng Yuan <lvhengy@gmail.com>
2023-12-09 00:22:00 -0600

    fix

--- preprocess/preprocess.cpp
+++ preprocess/preprocess.cpp
@@ -69,7 +69,7 @@ int main(int argc, char* argv[])
         // graph.Preprocess();
         // graph.writeGraphFile(filename);
         graph.writeDistGraphFile(filename);
-        graph.FennelPartition(filename);
+        graph.FennelPartition(filename, 4);
     }
     else 
     {
