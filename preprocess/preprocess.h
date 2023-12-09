d70d1f089c15958b51ce9b0e9c5b4c2269e7f669
Lyuheng Yuan <lvhengy@gmail.com>
2023-12-09 15:58:50 -0600

    fix

--- preprocess/preprocess.h
+++ preprocess/preprocess.h
@@ -678,7 +678,7 @@ void Graph::readGraphFile(const std::string &filename)
             uintV end;
             labelType label;
             int unknown;
-            file_in >> begin >> end >> label;
+            file_in >> begin >> end >> label >> unknown;
 
             uintV offset = row_ptrs_[begin] + neighbors_offset[begin];
             cols_[offset] = end;
