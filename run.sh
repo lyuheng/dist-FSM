50d02bce7f05993ab751937322640fcd90c3a41e
Lyuheng Yuan <lvhengy@gmail.com>
2023-12-29 16:09:07 -0600

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-mpiexec -hostfile $PBS_NODEFILE -n $1 ./run -file ../datasets/yeast_fsm -freq 280 -thread 32 -maxNodes 12
-mpiexec -hostfile $PBS_NODEFILE -n $1 ./run -file ../datasets/new_patent.lg -freq 24000 -thread 32 -lb 1
+#mpiexec -hostfile $PBS_NODEFILE -n $1 ./run -file ../datasets/yeast_fsm -freq 280 -thread 32 -maxNodes 12
+#mpiexec -hostfile $PBS_NODEFILE -n $1 ./run -file ../datasets/new_patent.lg -freq 24000 -thread 32 -lb 1
+mpiexec -hostfile $PBS_NODEFILE -n $1 ./run -file ../datasets/amazon.lg -freq 17000 -thread 32 -lb 1
