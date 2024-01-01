ff28ad9ce10759be17ce19b9fe079db363ad792c
Lyuheng Yuan <lvhengy@gmail.com>
2024-01-01 02:34:37 -0600

    fix

--- run.cpp
+++ run.cpp
@@ -11,11 +11,11 @@ using namespace std::chrono;
 int main(int argc, char *argv[])
 {   
     // launch a thread to record memory
-    // char outputfilePeakMem[1000];
-    // sprintf(outputfilePeakMem, "maxmem.txt");
-    // ofstream foutPeakMem(outputfilePeakMem);
-    // GetCurrentPid();
-	// thread t = thread(info, GetCurrentPid(), ref(foutPeakMem));
+    char outputfilePeakMem[1000];
+    sprintf(outputfilePeakMem, "maxmem.txt");
+    ofstream foutPeakMem(outputfilePeakMem);
+    GetCurrentPid();
+	thread t = thread(info, GetCurrentPid(), ref(foutPeakMem));
 
     init_worker(&argc, &argv);
 
@@ -120,9 +120,9 @@ int main(int argc, char *argv[])
         }
     }
 
-    // global_end_label_mem = false;
-    // t.join();
-	// foutPeakMem.close();
+    global_end_label_mem = false;
+    t.join();
+	foutPeakMem.close();
 
     worker.release_pattern_prog_map();
 
