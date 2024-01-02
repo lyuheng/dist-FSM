ccb39748566c07932950b6e7d3aebd8a6b299898
Lyuheng Yuan <lvhengy@gmail.com>
2024-01-01 23:18:35 -0600

    fix

--- core/cache_table.h
+++ core/cache_table.h
@@ -199,6 +199,7 @@ public:
 					{
 						bucket.unlock();
 						pos++; //current bucket has been checked, next time, start from next bucket
+						if(pos >= CONMAP_BUCKET_NUM) pos -= CONMAP_BUCKET_NUM;
 						return 0;
 					}
 				}
